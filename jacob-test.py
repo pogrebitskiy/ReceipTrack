01e82f68731306e4c40eb64c75aa6db7b61c4d63
Jacob Kulik <jakul238@gmail.com>
2022-11-12 09:55:01 -0500

    jacob file

--- jacob-test.py
+++ jacob-test.py
@@ -5,12 +5,15 @@ import cv2
 import numpy as np
 from pytesseract import Output
 import re
+import cv2
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\jakul\anaconda3\Library\bin\tesseract.exe'
 
-image = Image.open('receipt-ocr-original.jpg')
-#image = image.resize((300,150))
-print(image)
+img = cv2.imread('receipt-ocr-original.jpg')
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img= cv2.fastNlMeansDenoising(img, None, 20, 7, 21)
+img = cv2.fastNlMeansDenoisingColored(img,None,20,20,7,21)
 
 custom_config = r'-l eng --oem 3 --psm 6'
-text =pytesseract.image_to_string(image) # ERROR HERE
+text = pytesseract.image_to_string(img, config=custom_config, lang='eng')
 print(text)
