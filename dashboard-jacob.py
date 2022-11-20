0e289dc13d0493a86dc7c84fde6378022ce9663e
Jacob Kulik <jakul238@gmail.com>
2022-11-20 13:25:58 -0500

    jacob dashboard

---
+++ dashboard-jacob.py
@@ -0,0 +1,71 @@
+import io
+
+import dash_bootstrap_components as dbc
+from dash import Dash, html, dcc, Input, Output, State, dash_table
+from dash_bootstrap_templates import load_figure_template
+import datetime
+from mainj import read_receipt
+import base64
+from PIL import Image
+
+
+app = Dash(__name__,
+           external_stylesheets=[dbc.themes.LUX, 'https://codepen.io/chriddyp/pen/bWLwgP.css']
+           )
+load_figure_template('LUX')
+
+app.layout = html.Div([
+    html.H1('ReceipTrack', style={'textAlign': 'center'}),
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            html.Button('Upload Receipt(s)')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple=True
+    ),
+    html.Div(id='output-image-upload'),
+])
+
+
+@app.callback(Output('output-image-upload', 'children'),
+              Input('upload-image', 'contents'))
+def update_output(list_of_contents):
+    if list_of_contents is not None:
+        children = []
+        '''html.Div([html.Img(src=contents, style={'display': 'inline-block',
+                                                 'width': '50%',
+                                                 'margin-left': '10px'})]) for contents'''
+        for rec in list_of_contents:
+            # Split encoded string
+            content_type, content_string = rec.split(',')
+            rec = content_string
+
+            # Decode the string back to an image
+            img = Image.open(io.BytesIO(base64.b64decode(rec)))
+            img.save('temp.jpg')
+
+            # Read the redecoded image
+            rec = read_receipt('temp.jpg')
+
+            # Create a table from the receipt
+            children.append(html.Div([dbc.Table([html.Tbody([html.Tr([html.Td('Date'), html.Td(rec.date)]),
+                                                             html.Tr([html.Td('Phone Number'), html.Td(rec.phone)]),
+                                                             html.Tr([html.Td('Subtotal'), html.Td(rec.subtotal)]),
+                                                             html.Tr([html.Td('Total'), html.Td(rec.total)]),
+                                                             html.Tr([html.Td('Change Due'), html.Td(rec.change)])])])], style={'margin-left':'10px'}))
+            children.append(html.Hr())
+
+        return children
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ mainj.py
@@ -0,0 +1,24 @@
+import cv2
+import pytesseract
+from Receipt import Receipt
+import datetime
+import re
+import os
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\jakul\anaconda3\Library\bin\tesseract.exe'
+
+def read_receipt(filepath):
+    # Read image usingVOpenCS
+    img = cv2.imread(filepath, 0)
+    # configurations
+    config = r'-l eng --oem 3 --psm 6'
+
+    # Parse text using pytesseract
+    text = pytesseract.image_to_string(img, config=config, lang='eng')
+
+    # Create a Receipt object using the parsed text
+    return Receipt(text)
+
+if __name__ == '__main__':
+    rec = read_receipt('receipts/receipt2.jpeg')
+    print(rec)
